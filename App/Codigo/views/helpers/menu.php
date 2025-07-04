e1527f3969ccf567a5c1f8992633cb9fd7d9466c
BrianAb <144299964+BrianAbH@users.noreply.github.com>
2025-07-04 14:56:31 -0500

    Add files via upload

---
+++ App/Codigo/views/helpers/menu.php
@@ -0,0 +1,18 @@
+<!-- Barra de navegación principal -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/menu.css">
+</head>
+<body>
+    <nav class="menu">
+    <h1>G2 RepairTrack</h1>
+    <!-- Enlaces de navegación a distintas secciones -->
+        <a href="vistaCliente.php">Datos del Cliente</a>
+        <a href="vistaTecnico.php">Datos del Tecnico</a> 
+        <a href="vistaDispositivo.php">Datos Dispositivo</a> 
+        <a href="vistaReparaciones.php">Reparaciones</a>
+        <a href="/../../index.php">Cerrar Sesion</a>
+    </nav>
